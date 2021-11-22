32b1a6040680eaefa8efe6a54f1302c9cf6d254d
NovalFerdiansyahh <novalf999@gmail.com>
2021-11-23 01:37:06 +0700

    Menambahkan commit file

---
+++ DashboardDesign_24_NovalFerdiansyah/app/src/main/java/com/example/dashboarddesign_24_novalferdiansyah/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.exampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dashboarddesign_24_novalferdiansyah.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
